0b8840a5fb48faa21f3014ad243848236bc0b114
kimyooong <yhk@seoulauction.com>
2022-09-02 18:44:44 +0900

    스키마 파일 및 jar 파일 업로드

---
+++ result/db_schema.sql
@@ -0,0 +1,90 @@
+CREATE DATABASE  IF NOT EXISTS `dev` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `dev`;
+-- MySQL dump 10.13  Distrib 8.0.30, for Win64 (x86_64)
+--
+-- Host: localhost    Database: dev
+-- ------------------------------------------------------
+-- Server version	8.0.30
+
+-- username : root , password : toor
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `reset_password`
+--
+
+DROP TABLE IF EXISTS `reset_password`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `reset_password` (
+  `RP_ID` bigint NOT NULL AUTO_INCREMENT COMMENT '리셋 패스워드 고유 키',
+  `PHONE_NUMBER` varchar(100) DEFAULT NULL COMMENT '유저 폰 번호',
+  `RANDOM_NUMBER` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '패스워드 리셋에 필요한 랜덤 숫자',
+  `CONFIRM` bit(1) DEFAULT b'0' COMMENT '승인 여부',
+  `EXPIRE_DATE` datetime DEFAULT NULL COMMENT '만료 날짜',
+  `CREATE_DATE` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '생성 날짜',
+  PRIMARY KEY (`RP_ID`),
+  UNIQUE KEY `reset_password_un_phone_rn` (`PHONE_NUMBER`,`RANDOM_NUMBER`)
+) ENGINE=InnoDB AUTO_INCREMENT=49 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='패스워드 리셋 관련 테이블.';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `reset_password`
+--
+
+LOCK TABLES `reset_password` WRITE;
+/*!40000 ALTER TABLE `reset_password` DISABLE KEYS */;
+INSERT INTO `reset_password` VALUES (45,'jQvHkTLSLfulxknBzRnsWg==','305096',_binary '','2022-09-02 16:14:32','2022-09-02 15:54:32'),(46,'jQvHkTLSLfulxknBzRnsWg==','149646',_binary '','2022-09-02 16:14:50','2022-09-02 15:54:50'),(47,'dJ4zq5PVBIRbLgacK7kfMA==','445506',_binary '','2022-09-02 17:49:55','2022-09-02 17:29:55'),(48,'dJ4zq5PVBIRbLgacK7kfMA==','721940',_binary '','2022-09-02 17:50:47','2022-09-02 17:30:47');
+/*!40000 ALTER TABLE `reset_password` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tc_user`
+--
+
+DROP TABLE IF EXISTS `tc_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tc_user` (
+  `USER_ID` bigint NOT NULL AUTO_INCREMENT COMMENT '유저 고유 아이디',
+  `PASSWORD` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '유저 패스워드',
+  `EMAIL` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '유저 이메일',
+  `NICK_NAME` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '유저 닉 네임',
+  `NAME` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '유저 네임',
+  `PHONE_NUMBER` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '유저 전화번호',
+  `UPDATE_DATE` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '유저 업데이트 날짜',
+  `CREATE_DATE` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '유저 생성 날짜',
+  PRIMARY KEY (`USER_ID`),
+  UNIQUE KEY `tc_user_un_pn` (`PHONE_NUMBER`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='유저 정보 테이블.';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tc_user`
+--
+
+LOCK TABLES `tc_user` WRITE;
+/*!40000 ALTER TABLE `tc_user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tc_user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-09-02 17:43:28
